3d3d24567e1fb2d8c5e9daf3414659f2d1cdc96f
Amares03 <amaressabit0314@gmail.com>
2022-05-10 12:22:21 +0300

    added controller

---
+++ server/routes/router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const controller = require("../controller/controller");
+const render = require("../services/render");
+
+const router = express.Router();
+
+// API
+router.post("/api/user", controller.create);
+router.get("/icladdis/viewresult/:id", controller.find);
+router.get("/icladdis/viewresult/:id/detail", controller.detail);
+router.get("/icladdis/viewresult/:id/pdf", controller.pdfGenerate);
+router.get("/viewresult/ref=/:id", controller.findSample);
+// router.get("/viewresult/ref=/:id/pdf", controller.pdfGenerate);
+// router.get("/pdf/:id", controller.pdfGenerate);
+
+module.exports = router;
---
+++ server/services/render.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+exports.homeRout = async (req, res) => {
+  await axios
+    .get("https://iclilaboratory.herokuapp.com/icladdis/viewresult")
+    .then(function (response) {
+      res.render("index", { user: response.data });
+    })
+    .catch((err) => {
+      res.status(404).render("errorpage");
+    });
+};
