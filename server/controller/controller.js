0aa38d9c4ab13814d852e9c714bdfd122f304614
Amares03 <amaressabit0314@gmail.com>
2022-05-10 19:10:44 +0300

    set controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,5 +1,7 @@
 var userdb = require("../model/model");
 // const pdfService = require("../services/pdf-service");
+
+// create user
 exports.create = (req, res) => {
   if (!req.body) {
     res.status(400).send({ message: "user cannot be emity" });
@@ -25,6 +27,8 @@ exports.create = (req, res) => {
       res.status(500).send({ message: "error while saving" + err });
     });
 };
+
+// find user
 exports.find = (req, res) => {
   if (!req.body) {
     res.status(400).send({ message: "user cannot be emity" });
@@ -40,6 +44,42 @@ exports.find = (req, res) => {
     });
 };
 
+// update user
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Update Cannot be empity" });
+  }
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send(`User by id ${id} not Found`);
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(`Error While Updating User Data Information`);
+    });
+};
+
+// delete user
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Userdb.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `User by id ${id} is not found` });
+      } else {
+        res.status(200).send({ message: `user is Deleted Sucssuccfully` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `user not found` });
+    });
+};
+
+// generate pdf
 exports.pdfGenerate = (req, res, next) => {
   if (!req.body) {
     res.status(400).send({ message: "user cannot be emity" });
