02ef11e83732c4a3a80d6d3e00368fe96254f840
Amares03 <amaressabit0314@gmail.com>
2022-05-10 19:16:22 +0300

    set routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,10 @@ const controller = require("../controller/controller");
 const router = express.Router();
 
 // API
+router.post("/api/users", controller.create);
 router.get("/test-result-using-qr-code/:id", controller.find);
+router.put("api/users/:id", controller.update);
+router.delete("api/users/:id", controller.delete);
 router.get("/test-result-using-qr-code/:id/pdf", controller.pdfGenerate);
 
 module.exports = router;
