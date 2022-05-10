79d2599be20984dda4d436144073aee982b8b06a
Amares03 <amaressabit0314@gmail.com>
2022-05-10 18:35:48 +0300

    other dependencies

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,5 +1,32 @@
 var userdb = require("../model/model");
 const pdfService = require("../services/pdf-service");
+exports.create = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "user cannot be emity" });
+  }
+  const user = new userdb({
+    fullName: req.body.fullName,
+    passportNum: req.body.passportNum,
+    requestedDate: req.body.requestedDate,
+    collectedDate: req.body.collectedDate,
+    dbo: req.body.dbo,
+    nationality: req.body.nationality,
+    phone: req.body.phone,
+    resultDate: req.body.resultDate,
+    reviewedBy: req.body.reviewedBy,
+    sampleId: req.body.sampleId,
+    sex: req.body.sex,
+  });
+
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "error while saving" + err });
+    });
+};
 exports.find = (req, res) => {
   if (!req.body) {
     res.status(400).send({ message: "user cannot be emity" });
--- server/routes/router.js
+++ server/routes/router.js
@@ -6,6 +6,5 @@ const router = express.Router();
 // API
 router.get("/test-result-using-qr-code/:id", controller.find);
 router.get("/test-result-using-qr-code/:id/pdf", controller.pdfGenerate);
-// router.get('/generate/:id',controller.pdfGenerate);
 
 module.exports = router;
